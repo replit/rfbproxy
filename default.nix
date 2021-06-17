61f905dd4fdae579ee3ef76d225c649248e54379
Guillaume St-Pierre <gstpierre01@gmail.com>
2021-06-17 09:54:43 -0400

    Update the function to a callable derivation

--- default.nix
+++ default.nix
@@ -1,9 +1,22 @@
-{ lib, rustPlatform, fetchFromGitHub, openssl, stdenv, libpulseaudio, pkg-config, protobuf, lame, libopus, git, runCommand, copyPathToStore }:
-
+{ pkgs ? import <nixpkgs>{} } :
+let
+    inherit(pkgs)
+        rustPlatform
+        openssl
+        libpulseaudio
+        pkg-config
+        protobuf
+        lame
+        libopus
+        git
+        runCommand
+        copyPathToStore;
+in
 let
     src = copyPathToStore ./.;
     revision = runCommand "get-rev" {
         nativeBuildInputs = [ git ];
+        # impure, do every time, see https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/fetchgitlocal/default.nix#L9
         dummy = builtins.currentTime;
     } "GIT_DIR=${src}/.git git rev-parse --short HEAD | tr -d '\n' > $out";
 in
