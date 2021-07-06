fc842cbb57b92972db52a5d6d02b582d9af00d25
Connor Brewster <cbrewster@hey.com>
2021-07-06 12:55:16 -0600

    Add missing deps and format rfbproxy.nix

--- rfbproxy.nix
+++ rfbproxy.nix
@@ -1,42 +1,45 @@
-{stdenv, openssl, libpulseaudio, protobuf, lame, libopus, git, runCommand, copyPathToStore, rev} :
+{ stdenv, openssl, libpulseaudio, protobuf, lame, libopus, git, runCommand
+, copyPathToStore, rev, pkg-config, lib, defaultCrateOverrides, buildRustCrate
+, buildPackages, fetchurl }@pkgs:
 let
-    generatedBuild = import ./Cargo.nix {
-        inherit pkgs;
-        buildRustCrateForPkgs = pkgs: pkgs.buildRustCrate.override {
-            defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-                "opus-sys" = attrs: {
-                    nativeBuildInputs = [ pkg-config ];
-                    buildInputs = [ libopus ];
-                };
-                "libpulse-sys" = attrs: {
-                    nativeBuildInputs = [ pkg-config ];
-                    buildInputs = [ libpulseaudio ];
-                };
-                "libpulse-simple-sys" = attrs: {
-                    nativeBuildInputs = [ pkg-config ];
-                    buildInputs = [ libpulseaudio ];
-                };
-                rfbproxy = attrs: {
-                    buildInputs = [ openssl protobuf lame ];
-                    nativeBuildInputs = [ pkg-config ];
+  generatedBuild = import ./Cargo.nix {
+    inherit pkgs;
+    buildRustCrateForPkgs = pkgs:
+      pkgs.buildRustCrate.override {
+        defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+          "opus-sys" = attrs: {
+            nativeBuildInputs = [ pkg-config ];
+            buildInputs = [ libopus ];
+          };
+          "libpulse-sys" = attrs: {
+            nativeBuildInputs = [ pkg-config ];
+            buildInputs = [ libpulseaudio ];
+          };
+          "libpulse-simple-sys" = attrs: {
+            nativeBuildInputs = [ pkg-config ];
+            buildInputs = [ libpulseaudio ];
+          };
+          rfbproxy = attrs: {
+            buildInputs = [ openssl protobuf lame ];
+            nativeBuildInputs = [ pkg-config ];
 
-                    # needed for internal protobuf c wrapper library
-                    PROTOC = "${protobuf}/bin/protoc";
-                    PROTOC_INCLUDE = "${protobuf}/include";
-                };
-            };
+            # needed for internal protobuf c wrapper library
+            PROTOC = "${protobuf}/bin/protoc";
+            PROTOC_INCLUDE = "${protobuf}/include";
+          };
         };
-    };
+      };
+  };
 
-    crate2nix = generatedBuild.rootCrate.build;
+  crate2nix = generatedBuild.rootCrate.build;
 
 in stdenv.mkDerivation {
-    pname = "rfbproxy";
-    version = rev;
+  pname = "rfbproxy";
+  version = rev;
 
-    src = crate2nix;
+  src = crate2nix;
 
-    installPhase = ''
-        cp -r ${crate2nix} $out
-    '';
+  installPhase = ''
+    cp -r ${crate2nix} $out
+  '';
 }
